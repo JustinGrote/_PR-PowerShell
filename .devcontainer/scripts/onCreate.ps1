9848701d4575511cd3e40b84b9412285b5475022
Justin Grote <JustinGrote@users.noreply.github.com>
2024-03-04 17:17:55 -0800

    Fix duplicate symboliclink

--- .devcontainer/scripts/onCreate.ps1
+++ .devcontainer/scripts/onCreate.ps1
@@ -26,10 +26,6 @@ $ProgressPreference = 'Ignore'
 
 Get-ChildItem env:
 
-#Link Feature-Installed PowerShell to /usr/bin/pwsh, some vscode tasks hardcode this path
-New-Item -ItemType SymbolicLink -Path /usr/bin/pwsh -Value /usr/local/lib/pwsh/pwsh
-
-
 Import-Module ./build.psm1
 
 #For local devcontainers, sometimes permissions can end up mixed. Since Git 2.25, this can prevent git from working
